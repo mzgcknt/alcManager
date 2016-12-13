a0e3f3c4a74c5e43ad3c007401b0a50819fda431
mizoguchi kenta <347lionz@gmail.com>
2016-12-14 03:19:02 +0900

    変更前にとりあえずcommit

--- alcManager/FirstViewController.swift
+++ alcManager/FirstViewController.swift
@@ -54,21 +54,14 @@ class FirstViewController: UIViewController,ChartViewDelegate{
         
         let realm = try! Realm()
         
-        /*if daysCopy.isEmpty{    //最初に必ず通る
-            let formatter = DateFormatter()
-            formatter.dateFormat = "MM/dd"  //取得するフォーマットの設定
-            let now = Date()    //現在の日時を取得
-            let today = formatter.string(from: now)     //今日の日付 MM/dd
-            daysCopy.append(today)
-            valueCopy.append(0.0)
-        }*/
         print("はじめのdaysCopy setChart",daysCopy)
         print("はじめのvalueCopy setChart",valueCopy)
         for i in 0..<realm.objects(User.self).count {   //オブジェクト数を取得し、その回数分ループ
             let setObject = realm.object(ofType: User.self, forPrimaryKey: i)! //a?→a!でキャスト
             daysCopy.append(setObject.name)
-            valueCopy.append(round(setObject.value*10)/10)   //データベースに保存したものを格納
+            valueCopy.append(round(setObject.value*10)/10)   //データベースに保存していた数値を格納
         }
+
         print("daysCopy =" ,daysCopy)
         print("ValueCopy = ",valueCopy)
         
@@ -123,7 +116,6 @@ class FirstViewController: UIViewController,ChartViewDelegate{
         
         print("はじめのdaysCopy Contents",daysCopy)
         print("はじめのvalueCopy Contents",valueCopy)
-        
         if getAlcAmount == nil{    //最初に必ず通る
          daysCopy.append(today) //daysCopy ["12/14"]
          valueCopy.append(0.0)  //valueCopy [0.0]
@@ -138,13 +130,6 @@ class FirstViewController: UIViewController,ChartViewDelegate{
         print("append後のdaysCopy Contents",daysCopy)
         print("append後のvalueCopy Contents",valueCopy)
         
-        /*let a = getAlcAmount
-        if a != nil{
-            print("a!の値は ",a!)
-            valueCopy.append(Double(a!)!)
-            print("daysCopy",daysCopy)
-            print("valueCopy",valueCopy)
-        }*/
         let realm = try! Realm()    //デフォルトのrealmを取得
         for i in 0..<daysCopy.count{
             let user = User()   //インスタンス化
