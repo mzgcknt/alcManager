d6d370f191726726e94773fa3350303c314c9551
mizoguchi kenta <347lionz@gmail.com>
2016-12-15 10:23:10 +0900

    週と月のデータ管理が見えてきた

--- alcManager/FirstViewController.swift
+++ alcManager/FirstViewController.swift
@@ -15,8 +15,10 @@ class FirstViewController: UIViewController,ChartViewDelegate{
     @IBOutlet weak var barChartView: BarChartView!
     @IBOutlet weak var scrollView: UIScrollView!
     
-    var days:[String] = []
-    var alcValue:[Double] = []
+    @IBOutlet weak var wOm: UISegmentedControl!
+    
+    //var days:[String] = []
+    //var alcValue:[Double] = []
     var getAlcAmount:Double?
     
     override func viewDidLoad() {
@@ -34,10 +36,13 @@ class FirstViewController: UIViewController,ChartViewDelegate{
     }
     
     func setChart() { //Chartsの設定
-        
         var dataEntries: [BarChartDataEntry] = []
         let realm = try! Realm()
         let user = User()
+        var days:[String] = []
+        var alcValue:[Double] = []
+
+        print("realm.objects(User.self).last?.id",realm.objects(User.self).last?.id)    //最後はid=14
 
         for i in 0..<realm.objects(User.self).count {   //オブジェクト数を取得し、その回数分ループ
             let setObject = realm.object(ofType: User.self, forPrimaryKey: i)! //a?→a!でキャスト
@@ -46,7 +51,6 @@ class FirstViewController: UIViewController,ChartViewDelegate{
             alcValue.append(round(setObject.value*10)/10)   //データベースに保存していた数値を格納
         }
         if getAlcAmount == nil && user.value == 0.0{ //消すのは最初だけ
-
             user.deleteUser(id: realm.objects(User.self).count-1)   // count-1 = id
         }
         
@@ -76,6 +80,52 @@ class FirstViewController: UIViewController,ChartViewDelegate{
         barChartView.noDataText = "You need to provide data for the chart."
     }
     
+    func setChart(x:Int) { //Chartsの設定
+        var dataEntries: [BarChartDataEntry] = []
+        let realm = try! Realm()
+
+        var days:[String] = []
+        var alcValue:[Double] = []
+        
+        let predicate = realm.objects(User.self).filter("id >= 7")
+        print("predicate = ",predicate)
+        
+        
+        for i in predicate {   //オブジェクト数を取得し、その回数分ループ
+            let setObject = i //a?→a!でキャスト
+            print("setObject = ",setObject)
+            days.append(setObject.day)
+            alcValue.append(round(setObject.value*10)/10)   //データベースに保存していた数値を格納
+        }
+        /*if getAlcAmount == nil && user.value == 0.0{ //消すのは最初だけ
+            user.deleteUser(id: realm.objects(User.self).count-1)   // count-1 = id
+        }*/
+        
+        for i in 0..<days.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [alcValue[i]]) //values[i]をdouble型へ
+            dataEntries.append(dataEntry)
+        }
+        
+        let barchartDataSet = BarChartDataSet(values: dataEntries, label: "アルコール摂取量")
+        let ChartData = BarChartData(dataSet: barchartDataSet)       //BarChartをセット
+        
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: days)
+        barChartView.xAxis.setLabelCount(days.count, force: false)
+        barChartView.scaleXEnabled = false
+        //https://github.com/danielgindi/Charts/issues/19092
+        
+        let ll = ChartLimitLine(limit: 10.0, label: "Target")
+        barChartView.rightAxis.addLimitLine(ll) //Limit lineの設定
+        
+        barChartView.data = ChartData      //Viewへ追加
+        barChartView.xAxis.labelPosition = .bottom //x軸を下側に設定
+        barChartView.chartDescription?.text = ""    //descriptionのテキストを消す
+        barChartView.xAxis.drawGridLinesEnabled = false //x軸の方眼を非表示
+        barChartView.pinchZoomEnabled = false       //ピンチした際のズームを無効
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)  //背景色の設定
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)   //アニメーション
+        barChartView.noDataText = "You need to provide data for the chart."
+    }
     
     @IBAction func alcSelectBtn(_ sender: AnyObject) {  //とりあえずボタン押した時の処理
         
@@ -106,6 +156,14 @@ class FirstViewController: UIViewController,ChartViewDelegate{
     }
     
     @IBAction func weekOrMonth(_ sender: AnyObject) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            setChart(x:sender.selectedSegmentIndex)
+        case 1:
+            setChart(x:sender.selectedSegmentIndex)
+        default:
+            break
+        }
     }
     
 }
